ade2217487b71225f0b58b3e9c433dce95ef5f77
spidy-00-5 <gowtham398gs@gmail.com>
2026-01-19 01:07:53 +0530

    made new

--- flappybird/flappybird.c
+++ flappybird/flappybird.c
@@ -1,2 +1,96 @@
+#include"raylib.h"
 
+#define maxpipe 100 ;
+
+
+int main(){
+  
+  const int screenWidth  = 288;
+  const int screenHeight = 512;
+  
+
+  InitWndow(screenWidth,screenHeight,"flappy bird");
+
+  Texture2D background = LoadTexture("flappybird/asserts/flappy-bird-assets/sprites/background-day.png");
+  Texture2D base      = LoadTexture("flappybird/asserts/flappy-bird-assets/sprites/base.png");
+  Texture2D pipe_green = LoadTexture("flappybird/asserts/flappy-bird-assets/sprites/pipe_green.png");
+  Texture2D bird_downflap = LoadTexture("flappybird/asserts/flappy-bird-assets/sprites/redbird-downflap.png");
+  Texture2D bird_midflap = LoadTexture("flappybird/asserts/flappy-bird-assets/sprites/redbird-midflap.png");
+  Texture2D bird_upflap = LoadTexture("flappybird/asserts/flappy-bird-assets/sprites/redbird-upflap.png");
+
+  typedef struct bird {
+    
+    Texture2D sprites;
+    Vector2   position;
+    float     radius;
+
+  }bird;
+
+  typedef struct pipe_pair{
+    
+    Texture2D pipe;
+    Rectangle top;
+    Rectangle bottom;
+
+  }pipe_pair;
+
+  bird floppybird;
+  floppybird.spirit = bird_downflap;
+  floppybird.position = {1,1};
+  floppybird.radius = (bird_downflap.height+bird_downflap.width)/6.0f;
+
+  pipe_pair pipes[maxpipe];
+  int gap = 120;
+  float basespeed = 0.0f;
+  Vector2 center = (Vector2){floppybird.position.x + (floppybird.spirit.width)/2 ,floppybird.position.y + (floppybird.spirit.height)/2}
+
+   
+  for(int i = 0,i < 100; i++){
+    
+    pipes[i].pipe = pipe_green;
+    pipe[i].bottom = (Rectangle){(screenWidth/2)+i*gap,screenHeight - pipe_green.height + GetRandomValue(-60,10)}
+    pipe[i].top =(Rectangle){0+i*gap , pipe_green+GetRandomValue(0,20)}; 
+
+  }
+
+  SetTargetFPS(60);
+
+  while(!WindowShouldClose()){
+
+   basespeed -= 1.5f;
+   if(basespeed <= -base.width) basespeed = 0 ; 
+   
+   if(IsKeyPressed(KEY_SPACE)){
+      floppybird.spirit = bird_upflap;
+      floppybird.position.y += 5;
+    } 
+   if(floppybird.position.y <= 0 || floppybird.position.y >= screenHeight-base.height) return 0  ;
+    
+    
+    for(int i = 0;i< 100;i++){
+       if(CheckCollisionCircleRec(center , flappy.radius,pipes[i].top.y)) return 0 ;
+       if(CheckCollisionCircleRec(center , flappy.radius,pipes[i].bottom.y)) return 0 ;
+    }
+
+    BeginDrawing();
+
+      clearBackground(RAYWHITE);
+      DrawText(background,0,0,WHITE);
+
+      for(int i = 0; i < 100 ; i++){
+
+        DrawTextpro();
+        DrawTextEx();
+
+     }
+      
+      DrawText(floppybird.spirit,floppybird.position.x,floppybird.position.y,WHITE);
+      DrawTextEx( base ,(Vector2){basespeed + base.width,screenHeight-base.height},0.0f,0.2f,WHITE);
+      DrawTextEx( base ,(Vector2){basespeed ,screenHeight-base.height},0.0f,0.2f,WHITE);
+     
+  }
+
+   CloseWindow();
+  return 0 ;
+}
 
