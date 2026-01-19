748a424582c232b3c675ccb4f64f8fccc7e144fd
spidy-00-5 <gowtham398gs@gmail.com>
2026-01-19 13:55:33 +0530

    almost done

--- flappybird/flappybird.c
+++ flappybird/flappybird.c
@@ -17,6 +17,7 @@ int main(){
   Sound     point   = LoadSound("assets/flappy-bird-assets/audio/point.ogg");
   Sound     swoosh = LoadSound("assets/flappy-bird-assets/audio/swoosh.ogg");
   Sound     wing   = LoadSound("assets/flappy-bird-assets/audio/wing.ogg");
+  Sound     song   = LoadSound("assets/flappy-bird-assets/audio/song.mp4");
 
   Texture2D background = LoadTexture("assets/flappy-bird-assets/sprites/background-day.png");
   Texture2D base      = LoadTexture("assets/flappy-bird-assets/sprites/base.png");
@@ -25,6 +26,10 @@ int main(){
   Texture2D bird_midflap = LoadTexture("assets/flappy-bird-assets/sprites/redbird-midflap.png");
   Texture2D bird_upflap = LoadTexture("assets/flappy-bird-assets/sprites/redbird-upflap.png");
   Texture2D pipe_inverted = LoadTexture("assets/flappy-bird-assets/sprites/pipe-green-inverted.png");
+  Texture2D gameover = LoadTexture("assets/flappy-bird-assets/sprites/gameover.png");
+  Texture2D message  = LoadTexture("assets/flappy-bird-assets/sprites/message.png");
+  
+
   typedef struct bird {
     
     Texture2D spirites;
@@ -44,12 +49,15 @@ int main(){
 
   bird floppybird;
   floppybird.spirites = bird_downflap;
-  floppybird.position = (Vector2){10,10};
+  floppybird.position = (Vector2){10,screenHeight/2};
   floppybird.radius = (bird_downflap.height+bird_downflap.width)/4.0f;
 
   pipe_pair pipes[maxpipe];
-  int gap = 140;
+  int gap = 150;
   float basespeed = 0.0f;
+  bool game = false;
+  bool over  = false;
+  Vector2 gameoverpos = (Vector2){(screenWidth/2) - (gameover.width)/2,((screenHeight/2) - (gameover.height/2))-50};
 
    
   for(int i = 0 ; i < 100; i++){
@@ -57,8 +65,8 @@ int main(){
     pipes[i].pipe = pipe_green;
     pipes[i].pipe1 = pipe_inverted;
     float x = (screenWidth/2) + i * gap;
-    float randombottom = GetRandomValue(-80,-60); 
-    float randomtop = GetRandomValue(-130,-80);
+    float randombottom = GetRandomValue(-90,-70); 
+    float randomtop = GetRandomValue(-140,-90);
     pipes[i].bottom = (Rectangle){ x, (pipe_green.height + randombottom ), pipe_green.width, pipe_green.height};
     pipes[i].top = (Rectangle){ x ,- (pipe_green.height + randomtop), pipe_green.width, pipe_green.height };
   }
@@ -66,40 +74,56 @@ int main(){
   SetTargetFPS(60);
 
   while(!WindowShouldClose()){
-   
-   floppybird.position.y += 2;  
-   basespeed -= 1.5f;
-   if(basespeed <= -base.width) basespeed = 0 ; 
-    
 
-   Vector2 center = (Vector2){floppybird.position.x + (bird_downflap.width)/2 ,floppybird.position.y + (bird_downflap.height)/2};
-  
-   if(IsKeyPressed(KEY_SPACE)){
-      floppybird.spirites = bird_upflap;
-      floppybird.position.y -= 50;
-      PlaySound(wing);
-    } 
-   if(floppybird.position.y <= 0 || floppybird.position.y >= screenHeight-base.height) return 0  ;
-    
-    
-    for(int i = 0;i< maxpipe;i++){
-       if(CheckCollisionCircleRec(center , floppybird.radius,pipes[i].bottom)){
-        PlaySound(hit);
-        return 0 ;
-      }
-       if(CheckCollisionCircleRec(center , floppybird.radius,pipes[i].top)){
-        PlaySound(hit);
-        return 0 ;
-      }
-       pipes[i].bottom.x -= 1.5;
-       pipes[i].top.x -= 1.5;
-
+   if(IsKeyPressed(KEY_S)){
+      game = true ;
     }
+
+   if(game) {
+
+     PlaySound(song);
+
+     floppybird.position.y += 2;  
+     basespeed -= 1.5f;
+     if(basespeed <= -base.width) basespeed = 0 ; 
+    
+
+     Vector2 center = (Vector2){floppybird.position.x + (bird_downflap.width)/2 ,floppybird.position.y + (bird_downflap.height)/2};
+  
+     if(IsKeyPressed(KEY_SPACE)){
+         floppybird.spirites = bird_upflap;
+         floppybird.position.y -= 50;
+         PlaySound(wing);
+    }
+
+     if(floppybird.position.y <= 0 || floppybird.position.y >= screenHeight-base.height) return 0  ;
+    
+    
+     for(int i = 0;i< maxpipe;i++){
+         if(CheckCollisionCircleRec(center , floppybird.radius,pipes[i].bottom)){
+         PlaySound(hit);
+         over = true;
+          game = false;
+      }
+         if(CheckCollisionCircleRec(center , floppybird.radius,pipes[i].top)){
+         PlaySound(hit);
+         over = true;
+          game = false;
+      }
+
+        pipes[i].bottom.x -= 1.5;
+        pipes[i].top.x -= 1.5;
+
+     }
+
+  }
 
     BeginDrawing();
 
       ClearBackground(RAYWHITE);
       DrawTexture(background,0,0,WHITE);
+
+      if(over) DrawTexture(gameover,gameoverpos.x,(gameoverpos.y),WHITE);
 
       for(int i = 0; i < maxpipe ; i++){
         
